1644432436627067a89cc15c43ce3aa1c65726c7
Robert Kisiel <rkisiel@squiz.pl>
2019-10-08 08:17:45 +0200

    v3

--- cra-pwa-try/src/App.js
+++ cra-pwa-try/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Very v2 version
+          Very v3 version
         </p>
       </header>
     </div>
